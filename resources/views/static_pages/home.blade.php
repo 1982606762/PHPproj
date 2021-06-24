ec01185f0928ebf0edd5d49a8ae1e27196462e4d
WANG-Yun-1 <61615647+WANG-Yun-1@users.noreply.github.com>
2021-06-24 14:54:18 +0800

    Update home.blade.php

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,7 +4,7 @@
   <div class="jumbotron">
     <h1>欢迎使用预约系统</h1>
     <p>
-      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">注册</a>
     </p>
   </div>
 @stop
